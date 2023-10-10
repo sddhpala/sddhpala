ac33c1a6ebb6b188dbf31495abaabfdeec5561c6
Scott Fleming <sdflem@gmail.com>
2023-10-10 08:43:33 -0500

    Add Person model and scaffold pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_134003) do
+  create_table "people", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
